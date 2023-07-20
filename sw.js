56f2bcd45379d20a086dbdca486508a6261e05ea
MarkWezz <140047137+MarkWezz@users.noreply.github.com>
2023-07-20 12:22:38 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const staticCacheName = 's-app-v1'
+
+const assertUrls = [
+	'about_us.html',
+	'contact.html',
+	'feedback.html',
+	'feedback_sended.html',
+	'offline.html',
+	'popular.html',
+	'index.html',
+	'/js/app.js',
+	'style.css'
+]
+
+self.addEventListener(type:'install', listener:async event => {
+	const cache = await caches.open(staticCacheName)
+	await cache.addAll(assertUrls)
+})
+
+self.addEventListener(type:'activate', listener:event => {...})
+
+self.addEventListener(type:'fetch', listener:event => {
+	console.log('Fetch', event.request.url)
+	
+event.respondWith(cacheFirst(event.request))
+})
+
+async function cacheFirst(request) {
+	const cached = await caches.match(request)
+	return cached ?? await fetch(request)
+}
